d4faf24abc0cc8d3e155a12fb917cd7fa16761d8
Mohamad Hesari <mdhesari99@gmail.com>
2023-12-26 19:30:55 +0330

    refactor: readme and core

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -30,7 +30,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-//        dd(Storage::cloud());
+        //
     }
 
     private function repositories()
